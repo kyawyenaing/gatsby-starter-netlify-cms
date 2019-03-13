a08f0b6536e774ec3dfef3c6de1aa17946f4fa7c
Aries <thushin27@gmail.com>
2019-03-13 10:48:41 +0630

    add offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,6 +74,22 @@ module.exports = {
         purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
       },
     }, // must be after other CSS plugins
+    // nwew plugin i added
+    {
+      resolve: `gatsby-plugin-manifest`,
+            options: {
+              name: `GatsbyJS`,
+              short_name: `GatsbyJS`,
+              start_url: `/`,
+              background_color: `#f7f0eb`,
+              theme_color: `#a2466c`,
+              // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+              // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+              display: `standalone`,
+              icon: `src/img/social/facebook.svg`, // This path is relative to the root of the site.
+              include_favicon: true, // Include favicon
+            },
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
   // for avoiding CORS while developing Netlify Functions locally
